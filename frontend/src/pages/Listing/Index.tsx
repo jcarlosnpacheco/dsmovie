cce8117ac996352343246bae70b229ee2c593a10
Joao Pacheco <joao.pacheco.SPASSU_TECNOLOG@petrobras.com.br>
2022-01-14 11:51:38 -0300

    pagination

--- frontend/src/pages/Listing/Index.tsx
+++ frontend/src/pages/Listing/Index.tsx
@@ -33,9 +33,13 @@ function Listing() {
       });
   }, [pageNumber]);
 
+  const handlePageChange = (newPageNumber: number) => {
+    setPageNumber(newPageNumber);
+  };
+
   return (
     <>
-      <Pagination />
+      <Pagination page={page} onChange={handlePageChange} />
 
       <div className="container">
         <div className="row">
